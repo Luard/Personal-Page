b620670507e9f5abdd4a2dc24f9f85cd7561977d
Luard <luard.developer@gmail.com>
2022-12-11 16:53:30 +0100

    Customize main page with personal info

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,18 +12,29 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://luard.dev">Luard.dev!</a>
-        </h1>
-
         <Image
           priority
           src="/images/Luard.png"
-          height={144}
-          width={144}
+          height={250}
+          width={250}
           alt="Luard"
         />
-        
+        <h1 className={styles.title}>
+          Welcome to <a href="https://luard.dev">luard.dev!</a>
+        </h1>
+
+        <div className={styles.grid}>
+          <a href="https://github.com/luard" className={styles.card}>
+            <h2>My GitHub &rarr;</h2>
+            <p>Find in-depth information about my GitHub projects and repositories.</p>
+          </a>
+          <a href="https://padel-app.github.io/padel-app-docs/" className={styles.card}>
+            <h2>Padel App &rarr;</h2>
+            <p>Find in-depth information about the Padel App tech specs.</p>
+          </a>
+        </div>
+
+      {/*
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
@@ -59,20 +70,26 @@ export default function Home() {
             </p>
           </a>
         </div>
+        */}
       </main>
-
+      {/*
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://luard.dev"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image src="/images/luard.png"
+              alt="Luard"
+              width={16}
+              height={16}
+            />
           </span>
         </a>
       </footer>
+      */}
     </div>
   )
 }
