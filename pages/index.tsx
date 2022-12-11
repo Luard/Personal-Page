d545c3b7bc7d76b6da4b93a96832a88d47294c4a
Luard <luard.developer@gmail.com>
2022-12-11 17:00:06 +0100

    Replace favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <Head>
         <title>Luard</title>
         <meta name="description" content="Luard personal page" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/Luard.png" />
       </Head>
 
       <main className={styles.main}>
